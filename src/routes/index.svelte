837e6751942157f0cac1616669299ae1090db4b6
jptraber <jptraber@gmail.com>
2022-06-10 23:53:28 +0000

    appy css styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,43 @@
 <script>
-    import TodoItem from"../lib/todo-item.svelte" // "../" up directory, "$" finds folder
+    import TodoItem from"../lib/todo-item.svelte" // "../" one up directory, "$" finds folder
 
     const title = "Todo";
 </script>
+
+<style>
+    .todos {
+        width: 100%;
+        max-width: 42rem;
+        margin: 4rem auto 0 auto ;
+    }
+    .new{
+        margin: 0 0 0.5rem 0;
+    }
+
+    .new input{
+        font-size: 28px;
+        width: 100%;
+        padding: 0.5em 1em 0.3em 1em;
+        box-sizing: border-box;
+        background: rgba(255,255,255,0.05);
+        border-radius: 8px;
+        text-align: center;
+    }
+</style>
 
 <svelte:head>
     <title>{title}</title>
 </svelte:head>
 
+<div class="todos">
 <h1>{title}</h1>
 
-<form action="" method="">
+<form action="" method="" class="new">
     <input type="text" name="test" aria-label="Add a todo" placeholder="+ type to add a todo">
 </form>
 
 <TodoItem />
 <TodoItem />
 <TodoItem />
+
+</div>
